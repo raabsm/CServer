314f80049b01ad12387ebd2465b53567c00f7c2e
Samuel Raab <smr2218@columbia.edu>
2019-11-12 21:45:18 -0500

    minore changes

--- part2/http-server.c
+++ part2/http-server.c
@@ -15,6 +15,9 @@ static void die(const char *s) { perror(s); exit(1); }
 
 int main(int argc, char **argv)
 {
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+        die("signal() failed");
+ 
     if (argc != 5) {
         fprintf(stderr, "usage: %s <server_port> <web_root> <mdb-lookup-host> <mdb-lookup-port>\n", argv[0]);
         exit(1);
@@ -50,7 +53,7 @@ int main(int argc, char **argv)
     int clntsock;
     socklen_t clntlen;
     struct sockaddr_in clntaddr;
-    FILE *fp;
+    FILE *fd;
     char buf[4096];
 
     while(1){
@@ -60,8 +63,27 @@ int main(int argc, char **argv)
         if((clntsock =  accept(servsock, (struct sockaddr *) &clntaddr, &clntlen)) < 0)
             die("accept failed");
 
-
+        if ((fd = fdopen(clntsock, "r")) == NULL)
+            die("fdopen failed");       
         
+        if (fgets(buf, sizeof(buf), fd) == NULL) {
+            if (ferror(fd))
+                die("IO error");
+            else {
+                fclose(fd);
+                continue;
+            }
+        } 
+        
+        if(strncmp("GET ", buf, 4) != 0){
+            printf("failed some shit\n");
+        }
+        char *token_separators = "\t \r\n"; // tab, space, new line
+        char *method = strtok(buf, token_separators);
+        char *requestURI = strtok(NULL, token_separators);
+        char *httpVersion = strtok(NULL, token_separators);
+        printf("%s, %s, %s\n", method, requestURI, httpVersion);
+        close(fd);
 
 
 
